7aa311f7d907b4423369ef02d03c2d3f54009a97
Will B <wbigelow000@gmail.com>
2017-01-19 20:01:06 -0800

    Update Bluetooth_and_motors.ino

--- Prototyping/Rover/BabyCode/WillRyanNickElizabeth/Bluetooth_and_motors.ino
+++ Prototyping/Rover/BabyCode/WillRyanNickElizabeth/Bluetooth_and_motors.ino
@@ -20,22 +20,36 @@ void setup() {
   motor3->setSpeed(100);
   motor4->setSpeed(100);
   bluetooth.begin(9600);
-  bluetooth.println("Initializing");
+  bluetooth.println("Initialized");
 }
 
 void loop() {
   char toSend = (char)bluetooth.read();
-  Serial.print(toSend);
-  if(toSend == 'f') {
+  Serial.println(toSend);
+  if(toSend == 'f') { // forward
     motor1->run(BACKWARD);
     motor2->run(FORWARD);
     motor3->run(BACKWARD);
     motor4->run(FORWARD);
-  } else if(toSend == 's') {
+  } else if(toSend == 's') { // stop
     motor1->run(RELEASE);
     motor2->run(RELEASE);
     motor3->run(RELEASE);
     motor4->run(RELEASE);
+  } else if(toSend == 'r') { // reverse
+    motor1->run(FORWARD);
+    motor2->run(BACKWARD);
+    motor3->run(FORWARD);
+    motor4->run(BACKWARD);
+  } else if(toSend == 'b') { // boost
+    motor1->setSpeed(255);
+    motor2->setSpeed(255);
+    motor3->setSpeed(255);
+    motor4->setSpeed(255);
+  } else if(toSend == 'n') { // no boost
+    motor1->setSpeed(100);
+    motor2->setSpeed(100);
+    motor3->setSpeed(100);
+    motor4->setSpeed(100);
   }
 }
-
