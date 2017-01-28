f64c78ed03cc24f14f790c99b1e8ca2f697b1e6a
Jaden Bottemiller <jadenbottemiller@gmail.com>
2017-01-28 11:09:46 -0800

    Prep Distance Sensor test

--- Science/Sensors.py
+++ Science/Sensors.py
@@ -15,6 +15,7 @@ DIST_ADDR = 0x52
 #Create Sensors
 UV_Sens = UV.UV(UV_ADDR_LSB)
 Therm = Thermocouple.Thermocouple(PinClock, PinChipSel, PinDataIn)
+Dist = DistanceSensor(DIST_ADDR)
 pidCtrl = PID.PID(1, 1, 1)
 
 #Setup Sensors
@@ -22,12 +23,8 @@ UV_Sens.setup(2)
 
 print('Press Ctrl-C to quit.')
 
-itCount = 0
-test = 0
 
 while True:
-
-    pidCtrl.setTarget(100)
 
     # Read Sensor Data
     time.sleep(0.01)
@@ -35,13 +32,10 @@ while True:
     time.sleep(0.01)
     internal = Therm.getInternalTemp()
     uvData = UV_Sens.getData()
+    distData = Dist.getData()
 
-    if(itCount % 23 == 0):
-        test += 1
-
-    pidCtrl.run(test)
-
-    sys.stdout.write('{0}, '.format(pidCtrl.getOutput()))
+    sys.stdout.write('{0}, '.format(distData))
+    #sys.stdout.write('{0}, '.format(pidCtrl.getOutput()))
     #sys.stdout.write('{0:{fill}16b} ({0}),'.format(uvData, fill='0'))
     #sys.stdout.write(time.strftime("%Y-%m-%d %H:%M:%S,"))
     #sys.stdout.write('{0:0.2F};'.format(temp))
