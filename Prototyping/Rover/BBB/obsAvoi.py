6d1a942e47f3a66880b1ab5b08bcb43022164d06
Benton Kwong <Benton Kwong>
2017-02-16 18:59:57 -0800

    Updated obsAvoi.py

--- Prototyping/Rover/BBB/obsAvoi.py
+++ Prototyping/Rover/BBB/obsAvoi.py
@@ -1,6 +1,10 @@
+import Robot
 
 class obsAvoi :
+# Obstacle Avoidance
 # A work in progress
+# Assumes it will be placed within
+# Will work on making it better after Robot.py refactor
 
     def __init__(self, LP, RP):
         self.POT_LEFT = LP
@@ -8,8 +12,9 @@ class obsAvoi :
         self.POT_MIDDLE = (self.POT_LEFT + self.POT_RIGHT) / 2
 
     # TODO: Use sensor in front of rover to determine if there is a
-    #       an obstacle within a certain threshold in front
-    def isObsticle (self, distance):
+    #       an obstacle within a certain distance in front
+    #       Waiting for code from science subteam
+    def isObstacle (self, distance):
         return false
 
     # Looks around for obstacles and returns a heading without one
@@ -21,26 +26,35 @@ class obsAvoi :
         # Make the rover look as far left as possible
         while(readPot < POT_LEFT):
             robot.driveMoter(1, 20)
-            robot.driveMoter(3, 20)
+            robot.driveMoter(4, 20)
         robot.driveMoter(1, 0)
-        robot.driveMoter(3, 0)
+        robot.driveMoter(4, 0)
         # Look from left to right gathering obstacles in an arc ahead
         while(readPot > POT_Right):
             robot.driveMoter(1, -20)
-            robot.driveMoter(3, -20)
-            possibleHeadings.append((getMag(), isObsticle))
+            robot.driveMoter(4, -20)
+            possibleHeadings.append((getMag(), isObstacle))
             #TODO: Maybe? Add pause to slow read speed
         Robot.driveMoter(1, 0)
-        Robot.driveMoter(3, 0)
-        searchHeading = false
+        Robot.driveMoter(4, 0)
         # Determine path closest to center with no obstacle
-        while (searchHeading):
+        centerHeading = possibleHeadings.pop(self, int(len(possibleHeadings) / 2))[0]
+        inLoop = true
+        while (inLoop):
             middleHeading = int(len(possibleHeadings) / 2)
             tempHeading = possibleHeadings.pop(self, middleHeading)
             # Check for next value to the right of center
             if (not tempHeading[1]):
                 return tempHeading[0]
+            if(tempHeading[0] is None):
+                inLoop = false
             tempHeading = possibleHeadings.pop(self, middleHeading -1)
             # Check for next vale to the left of center
             if (not tempHeading[1]):
                 return tempHeading[0]
+            if __name__ == '__main__':
+                if (tempHeading[0] is None):
+                    inLoop = false
+        # If all scanned values have something in the way then ...
+        # TO BE CONTINUED
+
