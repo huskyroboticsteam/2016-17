a65195ac62dcdedaa0801393432bc40ab2d561e0
Theemathas Chirananthavat <theemathas@gmail.com>
2017-02-25 14:05:47 -0800

    Test magnetometer with motors on.

--- Prototyping/Rover/BBB/euler_test.py
+++ Prototyping/Rover/BBB/euler_test.py
@@ -1,6 +1,7 @@
 import BNO055
 import time
 import math
+import Robot
 
 def main():
     bno055 = BNO055.BNO055()
@@ -10,13 +11,21 @@ def main():
         return
     with open('calibration_data.txt', 'r') as f:
         bno055.set_calibration(map(int, f.read().split(' ')))
-    while True:
-        print 'calibration status:', bno055.get_calibration_status()
-        print 'euler:', bno055.read_euler()
-        magnetometer = bno055.read_magnetometer()
-        print 'magnetometer:', magnetometer
-        print 'magnetormeter angle:', math.degrees(math.atan2(magnetometer[1], magnetometer[0]))
-        time.sleep(0.1)
+    rob = Robot.Robot()
+    for i in range(1, 5):
+        rob.driveMotor(i, 200)
+    try:
+        while True:
+            print 'calibration status:', bno055.get_calibration_status()
+            print 'euler:', bno055.read_euler()
+            magnetometer = bno055.read_magnetometer()
+            print 'magnetometer:', magnetometer
+            print 'magnetometer angle:', math.degrees(math.atan2(magnetometer[1], magnetometer[0]))
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    for i in range(1, 5):
+        rob.stopMotor(i)
 
 
 if __name__ == "__main__":
