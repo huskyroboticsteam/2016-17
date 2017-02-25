9bcfeefb6b2ee5986f31c76712db799109a249c5
Theemathas Chirananthavat <theemathas@gmail.com>
2017-02-25 11:20:13 -0800

    Add calibration_save.py

---
+++ Prototyping/Rover/BBB/calibration_save.py
@@ -0,0 +1,27 @@
+import BNO055
+
+def main():
+    bno055 = BNO055.BNO055()
+    init_success = bno055.begin()
+    if not init_success:
+        print 'cannot initialize BNO055'
+        return
+    status = (0, 0, 0, 0)
+    try:
+        while not (status[0] == 3 and status[3] == 3):
+            status = bno055.get_calibration_status()
+            print status
+    except KeyboardInterrupt:
+        pass
+    calibration = bno055.get_calibration()
+    assert len(calibration) == 22
+    print 'calibration =', calibration
+    choice = raw_input('save data? (y/n) ')
+    if choice[0] == 'y':
+        print 'saving calibration...'
+        with open('calibration_data.txt', 'w') as f:
+            f.write(' '.join(map(str, calibration)))
+        print 'done'
+
+if __name__ == "__main__":
+    main()
