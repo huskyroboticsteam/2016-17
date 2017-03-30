0a948687509e131628c9312a641764988a132e2b
Nathan Wreggit <obbay2@hotmail.com>
2017-03-30 16:02:24 -0700

    Removed unused methods.

--- Prototyping/BaseStation/ui/ui_components/map/Utility.py
+++ Prototyping/BaseStation/ui/ui_components/map/Utility.py
@@ -62,25 +62,5 @@ def is_valid_coord(coord):
         return -180 <= flo <= 180
 
 
-def convertToDecimal(deg, min, sec):
-    DD = deg + float(min)/60 + float(sec)/3600
-    return DD
-
-
-def decdeg2dms(dd):
-    negative = dd < 0
-    dd = abs(dd)
-    minutes, seconds = divmod(dd * 3600, 60)
-    degrees, minutes = divmod(minutes, 60)
-    if negative:
-        if degrees > 0:
-            degrees = -degrees
-        elif minutes > 0:
-            minutes = -minutes
-        else:
-            seconds = -seconds
-    return (degrees, minutes, seconds)
-
-
 def is_valid_file_name(name):
     return name != ""
