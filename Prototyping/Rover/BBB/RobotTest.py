143080b7fd514e37ad49d356876b5efaff00de69
Will Bigelow <Will@D-10-18-249-94.dhcp4.washington.edu>
2017-04-08 11:17:12 -0700

    updates

--- Prototyping/Rover/BBB/RobotTest.py
+++ Prototyping/Rover/BBB/RobotTest.py
@@ -28,8 +28,8 @@ class Robot(object):
     def __init__(self):
         ADC.setup()
         # setup i2c to motorshield
-        self.pwm = Adafruit_PCA9685.PCA9685(address=0x60, busnum=1)
-        self.pwm.set_pwm_freq(60)
+        # self.pwm = Adafruit_PCA9685.PCA9685(address=0x60, busnum=1)
+        # self.pwm.set_pwm_freq(60)
 
         self.pot_pid = PID.PID(-0.1, 0, 0)
 
