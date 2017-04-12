134e58c702f21234800b436d607ef1e5307938a9
Theemathas Chirananthavat <theemathas@gmail.com>
2017-04-11 18:32:27 -0700

    Split path_finding_test.py into path_finding_test.py and path_finding.py

---
+++ Prototyping/Rover/BBB/path_finding.py
@@ -0,0 +1,55 @@
+from shapely.geometry import Point, Polygon, MultiPolygon, LinearRing
+from shapely.ops import cascaded_union
+import pyvisgraph as vg
+
+# TODO: Integrate this code into main code.
+
+# TODO: Steer rover to follow this line.
+# Use PID loop to make the robot face towards the next point in path.
+
+
+def find_path(start, target, obstacles, buffer_width):
+    """
+    Finds a path from one point to another while not getting near obstacles
+    Assumes that the actual start and target points are outside of obstacles, but the values given might be inaccurate.
+    Args:
+        start, target (tuple of (float, float)): starting points and target points
+        obstacles (list of tuple of (float, float)): list of obstacles
+        buffer_width (float): Do not go this near to the obstacles
+    Returns (list of tuple of (float, float)): The path.
+    """
+    polygon_list = [Point(a).buffer(buffer_width, resolution=3) for a in obstacles]
+    union_all = cascaded_union(polygon_list)
+    if isinstance(union_all, Polygon):
+        union_all = MultiPolygon([union_all])
+    start_outside = list(_nearest_outside(Point(start), union_all).coords)[0]
+    target_outside = list(_nearest_outside(Point(target), union_all).coords)[0]
+    boundary_list = [[vg.Point(b[0], b[1]) for b in a.exterior.coords] for a in union_all]
+    for i in range(len(boundary_list)):
+        del boundary_list[i][-1]
+    g = vg.VisGraph()
+    g.build(boundary_list)
+    shortest = g.shortest_path(
+        vg.Point(start_outside[0], start_outside[1]),
+        vg.Point(target_outside[0], target_outside[1]))
+    return [(a.x, a.y) for a in shortest]
+
+
+def _nearest_outside(p, area, epsilon=0.00001):
+    """
+    Finds the point closest to point p that is outside of the area and at least epsilon away from it.
+    Assumes that the area doesn't have holes.
+    Internal use only.
+    Args:
+        p (Point): The point
+        area (MultiPolygon or Polygon): the area
+    Returns (Point): the desired point
+    """
+    area = area.buffer(epsilon)
+    if not area.contains(p):
+        return p
+    poly = [a for a in MultiPolygon(area) if a.contains(p)][0]
+    assert poly.contains(p)
+    poly_ext = LinearRing(poly.exterior.coords)
+    d = poly_ext.project(p)
+    return poly_ext.interpolate(d)
--- Prototyping/Rover/BBB/path_finding_test.py
+++ Prototyping/Rover/BBB/path_finding_test.py
@@ -1,61 +1,10 @@
-from shapely.geometry import Point, Polygon, MultiPolygon, LinearRing
-from shapely.ops import cascaded_union
-import pyvisgraph as vg
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.patches import Circle
 from random import random
-
-# TODO: Steer rover to follow this line.
-# Maybe use PID loop?
-
-
-def find_path(start, target, obstacles, buffer_width):
-    """
-    Finds a path from one point to another while not getting near obstacles
-    Assumes that the actual start and target points are outside of obstacles, but the values given might be inaccurate.
-    Args:
-        start, target (tuple of (float, float)): starting points and target points
-        obstacles (list of tuple of (float, float)): list of obstacles
-        buffer_width (float): Do not go this near to the obstacles
-    Returns (list of tuple of (float, float)): The path.
-    """
-    polygon_list = [Point(a).buffer(buffer_width, resolution=3) for a in obstacles]
-    union_all = cascaded_union(polygon_list)
-    if isinstance(union_all, Polygon):
-        union_all = MultiPolygon([union_all])
-    start_outside = list(nearest_outside(Point(start), union_all).coords)[0]
-    target_outside = list(nearest_outside(Point(target), union_all).coords)[0]
-    boundary_list = [[vg.Point(b[0], b[1]) for b in a.exterior.coords] for a in union_all]
-    for i in range(len(boundary_list)):
-        del boundary_list[i][-1]
-    g = vg.VisGraph()
-    g.build(boundary_list)
-    shortest = g.shortest_path(
-        vg.Point(start_outside[0], start_outside[1]),
-        vg.Point(target_outside[0], target_outside[1]))
-    return [(a.x, a.y) for a in shortest]
-
-
-def nearest_outside(p, area, epsilon = 0.00001):
-    """
-    Finds the point closest to point p that is outside of the area and at least epsilon away from it.
-    Assumes that the area doesn't have holes.
-    Args:
-        p (Point): The point
-        area (MultiPolygon or Polygon): the area
-    Returns (Point): the desired point
-    """
-    area = area.buffer(epsilon)
-    if not area.contains(p):
-        return p
-    poly = [a for a in MultiPolygon(area) if a.contains(p)][0]
-    assert poly.contains(p)
-    poly_ext = LinearRing(poly.exterior.coords)
-    d = poly_ext.project(p)
-    return poly_ext.interpolate(d)
+from path_finding import find_path
 
 
 def str_to_tuple_of_float(s):
