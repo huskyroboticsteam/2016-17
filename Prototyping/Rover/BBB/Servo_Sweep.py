759dab2f71036a4dc5e35641ff0efe818e0d1d55
Albert Kwong <Albert Kwong>
2017-04-18 19:18:42 -0700

    fixed bugs and made changes in Servo_Sweep.py

--- Prototyping/Rover/BBB/Servo_Sweep.py
+++ Prototyping/Rover/BBB/Servo_Sweep.py
@@ -5,15 +5,15 @@ class Servo_Sweep(object):
     def __init__(self):
         self.servo_pin = "P8_13"
         self.duty_min = 3
-        duty_max = 14.5
-        duty_span = self.duty_max - self.duty_min
+        self.duty_max = 14.5
+        self.duty_span = self.duty_max - self.duty_min
 
         PWM.start(self.servo_pin, (100 - self.duty_min), 60.0, 1)
-        self.left = 120
+        self.left = 179
         self.center = 90
-        self.right = 60
+        self.right = 1
         self.clockwise = False
-        self.currentAngle = 90
+        self.currentAngle = 90.0
 
     def move(self):
         # first checks to see if the servo has reached the ends and reverses
@@ -23,11 +23,11 @@ class Servo_Sweep(object):
             self.clockwise = False
         # move the servo
         if self.clockwise:
-            angle = self.currentAngle - 1
+            self.currentAngle = self.currentAngle - 0.005
         else:
-            angle = self.currentAngle + 1
-        angle_f = float(angle)
-        duty = 100 - ((angle_f / 180) * self.duty_span + self.duty_min)
+            self.currentAngle = self.currentAngle + 0.005
+        angle_f = float(self.currentAngle)
+        duty = 100 - ((self.currentAngle / 180) * self.duty_span + self.duty_min)
         PWM.set_duty_cycle(self.servo_pin, duty)
 
 def main ():
