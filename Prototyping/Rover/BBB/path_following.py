9a8b031b6743578c71920575e6e87581027c63f1
Theemathas Chirananthavat <theemathas@gmail.com>
2017-05-06 11:21:10 -0700

    Delete incorrect comment.

--- Prototyping/Rover/BBB/path_following.py
+++ Prototyping/Rover/BBB/path_following.py
@@ -16,7 +16,6 @@ class PathFollower:
         # TODO fine tune this
         self.pid = PID(0.5, 0.0, 0.0)
         self.pid.setTarget(0.0)
-        # Note that .000005 is about 1 meter in terms of lat/longitude
         self.position_epsilon = 1
 
     def go(self, location, heading):
