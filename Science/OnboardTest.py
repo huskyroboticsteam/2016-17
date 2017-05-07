8da60a4439bfab2341b72222497f542d487bf742
Jaden Bottemiller <jadenbottemiller@gmail.com>
2017-05-06 18:06:09 -0700

    Did some onboard testing

--- Science/OnboardTest.py
+++ Science/OnboardTest.py
@@ -1,14 +1,14 @@
-import time
-from Motor import TalonMC
+import sys
+import Adafruit_BBIO.GPIO as GPIO
 
-MotorTest = TalonMC("P8_13")
 
-MotorTest.enable()
-MotorTest.set(1.0)
+def testEnc(channel, it=1000):
+    GPIO.setup(channel, GPIO.IN, pull_up_down=1)
+    for i in range(0, it):
+        if GPIO.input(channel):
+            sys.stdout.write("HIGH")
+        else:
+            sys.stdout.write("LOW")
+        sys.stdout.write("\n")
 
-for n in range(0, 100):
-    MotorTest.set(n / 100.0)
-    time.sleep(0.5)
-
-time.sleep(5)
-MotorTest.stopAll()
+testEnc("P9_22", 1000)
