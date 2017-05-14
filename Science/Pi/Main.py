903321a30f566881f5ae880ec1e7bf6c32f5c927
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-05-13 18:59:47 -0700

    Continued Working on Autofocus
    Trying a different algorithm. I think I know where I'm heading with this
    now.

--- Science/Pi/Main.py
+++ Science/Pi/Main.py
@@ -2,47 +2,80 @@ from PIL import Image
 import sys
 import math
 
-"""
-    Cuts out the specified part of the image, and translates RGB into a single value to prepare for sharpness calculations.
-"""
+# Cuts out the specified part of the image, and translates RGB into a single value to prepare for sharpness calculations.
 def PrepareImageData(ImgData, StartX, StartY, EndX, EndY):
     Output = [[0 for Y in range(StartY, EndY + 1)] for X in range(StartX, EndX + 1)];
     for Y in range(StartY, EndY):
         for X in range(StartX, EndX):
-            #sys.stdout.write("At " + str(X) + "->" + str(X - StartX) + ", " + str(Y) + "->" + str(Y - StartY) + "\n")
-            Pixel = ImgData[X, Y];
-            Output[X - StartX][Y - StartY] = (Pixel[0]) + ((Pixel[1]) << 8) + ((Pixel[2]) << 16);
+            #Pixel = ImgData[X, Y];
+            Output[X - StartX][Y - StartY] = ImgData[X, Y]#(Pixel[0]) + ((Pixel[1]) << 8) + ((Pixel[2]) << 16);
     return Output;
 
-def GetSharpness(ImgData, Width, Height):
+# Trying a few algorithms from this paper: http://amnl.mie.utoronto.ca/data/J25.pdf
+
+# Calculates the sharpness of a prepared data set.
+def GetSharpnessBasic(ImgData, Width, Height):
     Sum = 0;
     for Y in range(0, Height - 1):
         for X in range(0, Width - 1):
-            #if (X > (Width - 10)) or (Y > (Height - 10)):
-                #sys.stdout.write(str(X) + " " + str(Y) + "\n");
-
-            #sys.stdout.write(str(ImgData));
-            Sum += (ImgData[X+1][Y] - ImgData[X][Y]) ** 2;
-            Sum += (ImgData[X+1][Y] - ImgData[X][Y]) ** 2;
+            Sum += ((ImgData[X+1][Y][0] - ImgData[X][Y][0]) ** 2);
+            Sum += ((ImgData[X+1][Y][1] - ImgData[X][Y][1]) ** 2);
+            Sum += ((ImgData[X+1][Y][2] - ImgData[X][Y][2]) ** 2);
+            Sum += ((ImgData[X][Y][0] - ImgData[X][Y+1][0]) ** 2);
+            Sum += ((ImgData[X][Y][1] - ImgData[X][Y+1][1]) ** 2);
+            Sum += ((ImgData[X][Y][2] - ImgData[X][Y+1][2]) ** 2);
     return Sum;
 
-Images = ["Pi/test001.jpg", "Pi/test002.jpg", "Pi/test003.jpg", "Pi/test004.jpg", "Pi/test005.jpg", "Pi/test006.jpg", "Pi/test007.jpg", "Pi/test008.jpg", "Pi/test009.jpg", "Pi/test010.jpg", "Pi/test011.jpg", "Pi/test012.jpg"];
+def GetSharpnessVariance(ImgData, Width, Height):
+    SumsMean = [0, 0, 0];
+    for Y in range(0, Height):
+        for X in range(0, Width):
+            SumsMean[0] += ImgData[X][Y][0];
+            SumsMean[1] += ImgData[X][Y][1];
+            SumsMean[2] += ImgData[X][Y][2];
+
+    MeanInt = [0, 0, 0];
+    MeanInt[0] = (SumsMean[0] / (Height * Width));
+    MeanInt[1] = (SumsMean[1] / (Height * Width));
+    MeanInt[2] = (SumsMean[2] / (Height * Width));
+    #sys.stdout.write("Mean Colour: " + str(MeanInt[0]) + "," + str(MeanInt[1]) + "," + str(MeanInt[2]));
+
+    SumsVar = [0, 0, 0];
+    for Y in range(0, Height):
+        for X in range(0, Width):
+            SumsVar[0] += (ImgData[X][Y][0] - MeanInt[0]) ** 2;
+            SumsVar[1] += (ImgData[X][Y][1] - MeanInt[1]) ** 2;
+            SumsVar[2] += (ImgData[X][Y][2] - MeanInt[2]) ** 2;
+    SumsVar[0] /= (Width * Height);
+    SumsVar[1] /= (Width * Height);
+    SumsVar[2] /= (Width * Height);
+    return SumsVar[0] + SumsVar[1] + SumsVar[2];
+
+
+Images = [];
+for I in range(40, 53):
+    Images += ["Pi/test0" + str(I) + ".jpg"];
 
 for File in Images:
-    sys.stdout.write("=== Image: " + File + " ===\n");
+    #sys.stdout.write("=== Image: " + File + " ===\n");
+    # Opens the image and gets basic parameters.
     ImgObj = Image.open(File);
     ImgDataRaw = ImgObj.load();
     SizeRaw = ImgObj.size;
-    sys.stdout.write("Raw dimensions: [W:" + str(SizeRaw[0]) + " H:" + str(SizeRaw[1]) + "]\n");
+    #sys.stdout.write("Raw dimensions: [W:" + str(SizeRaw[0]) + " H:" + str(SizeRaw[1]) + "]\n");
 
-    Left = (SizeRaw[0] * 1/3);
-    Right = (SizeRaw[0] * 2/3);
-    Top = (SizeRaw[1] * 1/3);
-    Bottom = (SizeRaw[1] * 2/3);
+    # The region that will be checked for sharpness.
+    Left = 220#(SizeRaw[0] * 1/3);
+    Right = 1350#(SizeRaw[0] * 2/3);
+    Top = 150#(SizeRaw[1] * 1/3);
+    Bottom = 675#(SizeRaw[1] * 2/3);
     Size = (Right - Left), (Bottom - Top);
 
-    sys.stdout.write("Shrinking to [W:" + str(Size[0]) + " H:" + str(Size[1]) + "] by using [X:" + str(Left) + "->" + str(Right) + "],[Y:" + str(Top) + "->" + str(Bottom) + "]\n");
+    # Shrinks the data to the relevant region, and translates RGB into a single value for easier calculation.
+    #sys.stdout.write("Shrinking to [W:" + str(Size[0]) + " H:" + str(Size[1]) + "] by using [X:" + str(Left) + "->" + str(Right) + "],[Y:" + str(Top) + "->" + str(Bottom) + "]\n");
     ImgData = PrepareImageData(ImgDataRaw, Left, Top, Right, Bottom);
 
-    Sharpness = GetSharpness(ImgData, Size[0], Size[1]);
-    sys.stdout.write("F:" + str(Sharpness) + "\n\n");
+    SharpnessBas = GetSharpnessBasic(ImgData, Size[0], Size[1]);
+    sys.stdout.write(str(SharpnessBas) + "\n");
+    SharpnessVar = GetSharpnessVariance(ImgData, Size[0], Size[1]);
+    sys.stdout.write(str(SharpnessVar) + "\n\n");
