898047fe295c25b2b7cedb14cf45937a34155a1f
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-05-13 19:10:39 -0700

    Cleaned uf AF code, preparing for multithreading.
    It's slow. Let's make it fast.

--- Science/Pi/Main.py
+++ Science/Pi/Main.py
@@ -2,13 +2,14 @@ from PIL import Image
 import sys
 import math
 
-# Cuts out the specified part of the image, and translates RGB into a single value to prepare for sharpness calculations.
+Debug = False
+
+# Cuts out the specified part of the image to prepare for sharpness calculations.
 def PrepareImageData(ImgData, StartX, StartY, EndX, EndY):
     Output = [[0 for Y in range(StartY, EndY + 1)] for X in range(StartX, EndX + 1)];
     for Y in range(StartY, EndY):
         for X in range(StartX, EndX):
-            #Pixel = ImgData[X, Y];
-            Output[X - StartX][Y - StartY] = ImgData[X, Y]#(Pixel[0]) + ((Pixel[1]) << 8) + ((Pixel[2]) << 16);
+            Output[X - StartX][Y - StartY] = ImgData[X, Y]
     return Output;
 
 # Trying a few algorithms from this paper: http://amnl.mie.utoronto.ca/data/J25.pdf
@@ -38,7 +39,8 @@ def GetSharpnessVariance(ImgData, Width, Height):
     MeanInt[0] = (SumsMean[0] / (Height * Width));
     MeanInt[1] = (SumsMean[1] / (Height * Width));
     MeanInt[2] = (SumsMean[2] / (Height * Width));
-    #sys.stdout.write("Mean Colour: " + str(MeanInt[0]) + "," + str(MeanInt[1]) + "," + str(MeanInt[2]));
+    if Debug:
+        sys.stdout.write("Mean Colour: " + str(MeanInt[0]) + "," + str(MeanInt[1]) + "," + str(MeanInt[2]));
 
     SumsVar = [0, 0, 0];
     for Y in range(0, Height):
@@ -51,31 +53,36 @@ def GetSharpnessVariance(ImgData, Width, Height):
     SumsVar[2] /= (Width * Height);
     return SumsVar[0] + SumsVar[1] + SumsVar[2];
 
+def TestImageSet(Min, Max):
+    Images = [];
+    for I in range(Min, Max + 1):
+        Images += ["Pi/test0" + str(I) + ".jpg"];
 
-Images = [];
-for I in range(40, 53):
-    Images += ["Pi/test0" + str(I) + ".jpg"];
+    for File in Images:
+        if Debug:
+            sys.stdout.write("=== Image: " + File + " ===\n");
+        # Opens the image and gets basic parameters.
+        ImgObj = Image.open(File);
+        ImgDataRaw = ImgObj.load();
+        SizeRaw = ImgObj.size;
+        if Debug:
+            sys.stdout.write("Raw dimensions: [W:" + str(SizeRaw[0]) + " H:" + str(SizeRaw[1]) + "]\n");
 
-for File in Images:
-    #sys.stdout.write("=== Image: " + File + " ===\n");
-    # Opens the image and gets basic parameters.
-    ImgObj = Image.open(File);
-    ImgDataRaw = ImgObj.load();
-    SizeRaw = ImgObj.size;
-    #sys.stdout.write("Raw dimensions: [W:" + str(SizeRaw[0]) + " H:" + str(SizeRaw[1]) + "]\n");
+        # The region that will be checked for sharpness.
+        Left = (SizeRaw[0] * 1/3);
+        Right = (SizeRaw[0] * 2/3);
+        Top = (SizeRaw[1] * 1/3);
+        Bottom = (SizeRaw[1] * 2/3);
+        Size = (Right - Left), (Bottom - Top);
 
-    # The region that will be checked for sharpness.
-    Left = 220#(SizeRaw[0] * 1/3);
-    Right = 1350#(SizeRaw[0] * 2/3);
-    Top = 150#(SizeRaw[1] * 1/3);
-    Bottom = 675#(SizeRaw[1] * 2/3);
-    Size = (Right - Left), (Bottom - Top);
+        # Shrinks the data to the relevant region, and translates RGB into a single value for easier calculation.
+        if Debug:
+            sys.stdout.write("Shrinking to [W:" + str(Size[0]) + " H:" + str(Size[1]) + "] by using [X:" + str(Left) + "->" + str(Right) + "],[Y:" + str(Top) + "->" + str(Bottom) + "]\n");
+        ImgData = PrepareImageData(ImgDataRaw, Left, Top, Right, Bottom);
 
-    # Shrinks the data to the relevant region, and translates RGB into a single value for easier calculation.
-    #sys.stdout.write("Shrinking to [W:" + str(Size[0]) + " H:" + str(Size[1]) + "] by using [X:" + str(Left) + "->" + str(Right) + "],[Y:" + str(Top) + "->" + str(Bottom) + "]\n");
-    ImgData = PrepareImageData(ImgDataRaw, Left, Top, Right, Bottom);
+        SharpnessBas = GetSharpnessBasic(ImgData, Size[0], Size[1]);
+        sys.stdout.write(str(SharpnessBas) + "\n");
+        SharpnessVar = GetSharpnessVariance(ImgData, Size[0], Size[1]);
+        sys.stdout.write(str(SharpnessVar) + "\n\n");
 
-    SharpnessBas = GetSharpnessBasic(ImgData, Size[0], Size[1]);
-    sys.stdout.write(str(SharpnessBas) + "\n");
-    SharpnessVar = GetSharpnessVariance(ImgData, Size[0], Size[1]);
-    sys.stdout.write(str(SharpnessVar) + "\n\n");
+TestImageSet(30, 35);
