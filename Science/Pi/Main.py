2d509717186876a842a1759f727b4890cb33c502
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-05-13 20:35:22 -0700

    Multithreaded Image Preparation
    PLEASE DO NOT LOOK AT THE CODE :(
    For reasons that I cannot complrehend, Python does not allow me to
    return from threads. That's why there's so much evil hackery.

--- Science/Pi/Main.py
+++ Science/Pi/Main.py
@@ -1,16 +1,20 @@
 from PIL import Image
 import sys
 import math
+import threading
 
 Debug = False
 
 # Cuts out the specified part of the image to prepare for sharpness calculations.
-def PrepareImageData(ImgData, StartX, StartY, EndX, EndY):
-    Output = [[0 for Y in range(StartY, EndY + 1)] for X in range(StartX, EndX + 1)];
-    for Y in range(StartY, EndY):
-        for X in range(StartX, EndX):
-            Output[X - StartX][Y - StartY] = ImgData[X, Y]
-    return Output;
+def PrepareImageData(ImgData, StartX, StartY, EndX, EndY, Out):
+    #Output = [[0 for Y in range(StartY, EndY + 1)] for X in range(StartX, EndX + 1)];
+    for X in range(StartX, EndX):
+        Out[X - StartX] = [-1 for Yl in range(StartY, EndY + 1)]
+        for Y in range(StartY, EndY):
+            Out[X - StartX][Y - StartY] = ImgData[X, Y]
+    sys.stdout.write("Finished preparation thread, first: " + str(Out[0][0][0]) + "\n");
+    #Out = Output;
+    return Out;#put;
 
 # Trying a few algorithms from this paper: http://amnl.mie.utoronto.ca/data/J25.pdf
 
@@ -53,6 +57,30 @@ def GetSharpnessVariance(ImgData, Width, Height):
     SumsVar[2] /= (Width * Height);
     return SumsVar[0] + SumsVar[1] + SumsVar[2];
 
+# Creates and manages the threads to prepare image for processing. (PrepareImageData)
+def PrepImageAsync(ImgData, StartX, StartY, EndX, EndY, ThreadsX, ThreadsY, SizeX, SizeY):
+    Threads = [[0 for Y in range(0, ThreadsY)] for X in range(0, ThreadsX)];
+    Outputs = [[[] for Y in range(0, ThreadsY)] for X in range(0, ThreadsX)];
+    for Y in range(0, ThreadsY):
+        for X in range(0, ThreadsX):
+            StartX = (SizeX / ThreadsX) * X;
+            StartY = (SizeY / ThreadsY) * Y;
+            EndX = ((SizeX / ThreadsX) * (X + 1)) + 1;
+            EndY = ((SizeY / ThreadsY) * (Y + 1)) + 1;
+            Outputs[X][Y] = [[] for Xl in range(StartX, EndX + 1)];
+            Threads[X][Y] = threading.Thread(target=PrepareImageData, args=(ImgData, StartX, StartY, EndX, EndY, Outputs[X][Y]));
+
+    for Y in range(0, ThreadsY):
+        for X in range(0, ThreadsX):
+            Threads[X][Y].start();
+            sys.stdout.write("Started\n");
+            
+    for Y in range(0, ThreadsY):
+        for X in range(0, ThreadsX):
+            Threads[X][Y].join();
+    return Outputs;
+
+# Runs tests.
 def TestImageSet(Min, Max):
     Images = [];
     for I in range(Min, Max + 1):
@@ -85,4 +113,22 @@ def TestImageSet(Min, Max):
         SharpnessVar = GetSharpnessVariance(ImgData, Size[0], Size[1]);
         sys.stdout.write(str(SharpnessVar) + "\n\n");
 
-TestImageSet(30, 35);
+#TestImageSet(30, 35);
+
+
+# Opens the image and gets basic parameters.
+ImgObj = Image.open("Pi/test001.jpg");
+ImgDataRaw = ImgObj.load();
+SizeRaw = ImgObj.size;
+if Debug:
+    sys.stdout.write("Raw dimensions: [W:" + str(SizeRaw[0]) + " H:" + str(SizeRaw[1]) + "]\n");
+
+# The region that will be checked for sharpness.
+Left = (SizeRaw[0] * 1/3);
+Right = (SizeRaw[0] * 2/3);
+Top = (SizeRaw[1] * 1/3);
+Bottom = (SizeRaw[1] * 2/3);
+Size = (Right - Left), (Bottom - Top);
+
+ImgDataArr = PrepImageAsync(ImgDataRaw, Left, Top, Right, Bottom, 3, 3, Size[0], Size[1]);
+sys.stdout.write(str(ImgDataArr[0][0][0][0][0]) + "\n");
