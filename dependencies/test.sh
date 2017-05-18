d9ae00e8cd40e358c614e622e86cac69e252c64e
Nathan Wreggit <obbay2@hotmail.com>
2017-05-17 23:30:33 -0700

    Added powershell installer for python 3 windows dependencies for 2017-18 year.

--- dependencies/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -nc http://get.videolan.org/vlc/2.2.4/win32/vlc-2.2.4-win32.exe
-wget -nc http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.4/PyQt4-4.11.4-gpl-Py2.7-Qt4.8.7-x32.exe
-wget -nc https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi
