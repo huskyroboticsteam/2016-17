b151dd7827198d600f5a8d781b10387334a41c25
Theemathas Chirananthavat <theemathas@gmail.com>
2017-05-26 15:10:25 -0700

    Convert tabs to spaces.

--- Prototyping/Rover/BBB/basicAutonomous.py
+++ Prototyping/Rover/BBB/basicAutonomous.py
@@ -1,23 +1,24 @@
 import Utils
 
+
 class basicAutonomous:
 
-	def __init__(self):
-		self.target = None
-		self.position_epsilon = 3
+    def __init__(self):
+        self.target = None
+        self.position_epsilon = 3
 
-	def set_target(self, target):
-		self.target = target
+    def set_target(self, target):
+        self.target = target
 
-	def go(self, location, heading):
-		assert self.target is not None
-		desiredHeading = Utils.bearing(location, self.target)
-		if desiredHeading > 0:
-			return min(desiredHeading, 100)
-		return max(desiredHeading, -100)
+    def go(self, location, heading):
+        assert self.target is not None
+        desiredHeading = Utils.bearing(location, self.target)
+        if desiredHeading > 0:
+            return min(desiredHeading, 100)
+        return max(desiredHeading, -100)
 
-	def is_done(self, location):
-		if Utils.unscaledDist(self.target, location) <= self.position_epsilon:
-			return True
-		return False
+    def is_done(self, location):
+        if Utils.unscaledDist(self.target, location) <= self.position_epsilon:
+            return True
+        return False
 
