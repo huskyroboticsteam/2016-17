84419f816439523702d1cfadff545fdee123a49c
Theemathas Chirananthavat <theemathas@gmail.com>
2017-05-26 15:15:32 -0700

    Fix basicAutonomous.py

--- Prototyping/Rover/BBB/basicAutonomous.py
+++ Prototyping/Rover/BBB/basicAutonomous.py
@@ -12,10 +12,15 @@ class basicAutonomous:
 
     def go(self, location, heading):
         assert self.target is not None
-        desiredHeading = Utils.bearing(location, self.target)
-        if desiredHeading > 0:
-            return min(desiredHeading, 100)
-        return max(desiredHeading, -100)
+        desired_heading = Utils.bearing(location, self.target)
+        diff_heading = desired_heading - heading
+        while diff_heading > 180.0:
+            diff_heading -= 360.0
+        while diff_heading < -180.0:
+            diff_heading += 360.0
+        if diff_heading > 0:
+            return min(diff_heading, 100)
+        return max(diff_heading, -100)
 
     def is_done(self, location):
         if Utils.unscaledDist(self.target, location) <= self.position_epsilon:
